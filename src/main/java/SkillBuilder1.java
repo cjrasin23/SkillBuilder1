2fdf20a1e08b71f87d512f2ce18dbef44fba0372
cjrasin23 <cjrasin23@students.desu.edu>
2024-02-02 18:26:28 -0500

    SkillBuilder1.java

--- src/main/java/SkillBuilder1.java
+++ src/main/java/SkillBuilder1.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.sql.SQLOutput;
+
 
 /**
  * Skill Builder 0
@@ -9,11 +11,20 @@ import java.util.Scanner;
 public class SkillBuilder1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        //TODO : Replace this comment with your code
-    }
+        System.out.println("You're Jane's friend!");
+        System.out.println("What's your name?");
+        String name = input.nextLine();
+        System.out.println("Enter a floating-point number:");
+        double spice = input.nextDouble();
 
+        double spiceResult = (4.0 / 3.0) * Math.pow(2.0, (Math.sqrt(5) / Math.pow(spice, 3)));
+        System.out.println("Well " + name + ", the spice value resulted in " + spiceResult);
+        spiceResult = (double) ((int) (spiceResult * 100)) / 100;
+        System.out.println("And the converted value is " + spiceResult);
+    }
     public static void calcWallPaint() {
         Scanner input = new Scanner(System.in);
+
 
         final double squareFeetPerGallons = 350.0;
         final double gallonsPerCan = 1.0;
@@ -26,7 +37,15 @@ public class SkillBuilder1 {
         System.out.println("Enter wall width (feet):");
         double wallWidth = input.nextDouble();
 
-        // TODO: Calculate and output the wall's area
+        double wallArea = wallHeight * wallWidth;
+        System.out.println("Wall area: " + (int)wallArea + " square feet");
+
+        double paintNeeded = wallArea / squareFeetPerGallons;
+        String gallons = String.format("%.2f",paintNeeded);
+        System.out.println("Paint needed: " + gallons + " gallons");
+
+        int cansNeeded = (int) Math.ceil(paintNeeded / gallonsPerCan);
+        System.out.println("Cans needed: " + cansNeeded + " can(s)");
 
         // TODO: Calculate and output the amount of paint in gallons needed to paint the wall
 
